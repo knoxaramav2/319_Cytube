1accfd4f9157c72d8c8684cb3f9bfe1dc1c4ee72
Andrew <32649032+Andrew12000@users.noreply.github.com>
2017-10-14 21:35:14 -0500

    Implimented video playing, updated install.cmd, cleaned up package.json
    I added some code from https://github.com/daspinola/video-stream-sample, put a couple more things in install.cmd, added a simple video of the day button, removed a lot of items from package.json but also kept a copy if we needed it (feel free to delete the copy if we don't need it)

--- installPackages.cmd
+++ installPackages.cmd
@@ -6,5 +6,7 @@ nodemon server_file_.js
 npm install body-parser --save
 npm install cookie-parser --save
 npm install multer --save
+npm install mustache --save
+npm install ejs --save
 
 echo Done
--- site.js
+++ site.js
@@ -27,6 +27,9 @@ app.set('view engine', 'ejs');
 
 //const header = pug.compileFile('partials/template.pug');
 
+
+
+
 //constants
 var listen_port = 8080;
 
@@ -36,6 +39,41 @@ console.log(__dirname);
 app.get('/', function(req, res){
 	res.render("index");
 }); 
+
+app.get('/video', function(req, res) {
+	  const path = 'assets/rick.mp4'
+	  const stat = fs.statSync(path)
+	  const fileSize = stat.size
+	  const range = req.headers.range
+
+	  if (range) {
+	    const parts = range.replace(/bytes=/, "").split("-")
+	    const start = parseInt(parts[0], 10)
+	    const end = parts[1]
+	      ? parseInt(parts[1], 10)
+	      : fileSize-1
+
+	    const chunksize = (end-start)+1
+	    const file = fs.createReadStream(path, {start, end})
+	    const head = {
+	      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+	      'Accept-Ranges': 'bytes',
+	      'Content-Length': chunksize,
+	      'Content-Type': 'video/mp4',
+	    }
+
+	    res.writeHead(206, head)
+	    file.pipe(res)
+	  } else {
+	    const head = {
+	      'Content-Length': fileSize,
+	      'Content-Type': 'video/mp4',
+	    }
+	    res.writeHead(200, head)
+	    fs.createReadStream(path).pipe(res)
+	  }
+	});
+
 
 var server = app.listen(listen_port, function(){
 	var host = server.address().address;
