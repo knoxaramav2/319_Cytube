ea3bcbd8dd0db77045eeb40f8d55f514759ac534
= <wtscott@iastate.edu>
2017-10-17 02:52:32 +0100

    uploadable video... sort of

--- site.js
+++ site.js
@@ -7,6 +7,7 @@ var events = require('events');
 var path = require('path');
 var ejs = require('ejs');
 var multipart = require('connect-multiparty');
+//var multer = require('multer');
 var bodyParser = require('body-parser');
 
 //local requires
@@ -15,7 +16,9 @@ var utility = require('./util');
 //setup route objects
 const express = require('express');
 const app = express();
-var multiPartMiddleware = multipart();
+
+//var upload = multer({storage: storage});
+//var multiPartMiddleware = multipart();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -31,8 +34,23 @@ app.use(express.static('public')); //i.e http://.../images/Tux.png
 //app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "http://localhost");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+/*
+var storage = multer.diskStorage({ //multers disk storage settings
+	destination: function (req, file, cb) {
+		cb(null, './public/uploads/videos/')
+	},
+	filename: function (req, file, cb) {
+		var datetimestamp = Date.now();
+		cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
+	}
+});
 
-
+*/
 
 //constants
 var listen_port = 8080;
@@ -88,35 +106,61 @@ app.get('/upload', function(req, res) {
 	res.render("upload");
 });
 
-app.post('/file/upload', multiPartMiddleware, function(req, res) {
+//app.post('/file/upload', upload.single('photo'));
+
+
+/*
+var upload = multer({ //multer settings
+				storage: storage
+			}).single('file');
+
+app.post('/file/upload', function(req, res) {
+	console.log('upload');
+	upload(req,res,function(err){
+		if(err){
+			 console.log('err');
+			 res.json({error_code:1,err_desc:err});
+			 return;
+		}
+		 res.json({error_code:0,err_desc:null});
+	})
+});*/
+
+
+
+app.post('/file/upload', function(req, res, next) {
 	console.log(req.body);
-	console.log(req.files);
+	console.log(req['filename']);
 	
 	let videoPath = "./public/videos/";
-	let username = req.body['username'];
-	let fileinfo = req.files.file;
-	let filename = fileinfo['originalFilename'];
-	let localFileName = videoPath + utility.createVideoName(filename);
+	//let username = req.body['username'];
+	//let fileinfo = req.files.file;
+	//let filename = fileinfo['originalFilename'];
+	let localFileName = videoPath + utility.createVideoName("x.mp4");
 	let body = "";
 	
-	console.log(localFileName);
+	//console.log(localFileName);
 	
 	req.on('data', function(data) {
 		console.log('DATA');
 		body += data;
+		next();
 	});
 	
 	req.on('end', function(){
+		next();
 		console.log("asdasdasd");
 		fs.appendFile(localFileName, body, function(){
+			console.log('success');
 			res.end('SUCCESS');
 		});
+		
+		//res.end('a');
 	});
-	
 
-	
 	res.end('Video uploaded successfully');
 });
+
 
 var server = app.listen(listen_port, function(){
 	var host = server.address().address;
